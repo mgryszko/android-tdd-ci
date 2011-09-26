842fb4255890c5a6e62cc1a0f750747b73aaccc8
Marcin Gryszko <marcin@gryszko.net>
2011-09-27 00:17:20 +0200

    Simple test checking if TestNG works

---
+++ app/src/test/java/es/osoco/chirp/test/TestNGWorksTest.java
@@ -0,0 +1,12 @@
+package es.osoco.chirp.test;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class TestNGWorksTest {
+    @Test
+    public void test_testng_tests_can_be_executed_in_the_pc_vm() {
+        assertTrue(true);
+    }
+}
