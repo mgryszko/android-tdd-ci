0e5d85ed31403eb7b7b0a3f5c83c26cfea0808cd
Marcin Gryszko <marcin@gryszko.net>
2011-09-27 00:13:23 +0200

    Simple test checking if JUnit works

---
+++ app/src/test/java/es/osoco/chirp/test/JUnitWorksTest.java
@@ -0,0 +1,9 @@
+package es.osoco.chirp.test;
+
+import junit.framework.TestCase;
+
+public class JUnitWorksTest extends TestCase {
+    public void test_junit_tests_can_be_executed_in_the_pc_vm() {
+        assertTrue(true);
+    }
+}
